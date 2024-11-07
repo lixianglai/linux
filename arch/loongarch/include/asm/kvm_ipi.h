373a730c6ced6c883d85ff7873321917048a991a
Xianglai Li <lixianglai@loongson.cn>
2024-11-07 16:59:27 +0800

    LoongArch: KVM: Add IPI read and write function
    Add implementation of IPI interrupt controller's address space read and
    write function simulation.
    
    Signed-off-by: Min Zhou <zhoumin@loongson.cn>
    Signed-off-by: Tianrui Zhao <zhaotianrui@loongson.cn>
    Signed-off-by: Xianglai Li <lixianglai@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

--- arch/loongarch/include/asm/kvm_ipi.h
+++ arch/loongarch/include/asm/kvm_ipi.h
@@ -28,6 +28,19 @@ struct ipi_state {
 #define SMP_MAILBOX			0x1000
 #define KVM_IOCSR_IPI_ADDR_SIZE		0x160
 
+#define CORE_STATUS			0x000
+#define CORE_EN				0x004
+#define CORE_SET			0x008
+#define CORE_CLEAR			0x00c
+#define CORE_BUF_20			0x020
+#define CORE_BUF_28			0x028
+#define CORE_BUF_30			0x030
+#define CORE_BUF_38			0x038
+
+#define IOCSR_IPI_SEND			0x040
+#define IOCSR_MAIL_SEND			0x048
+#define IOCSR_ANY_SEND			0x158
+
 int kvm_loongarch_register_ipi_device(void);
 
 #endif
